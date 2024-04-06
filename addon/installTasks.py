eff893aa58fb6eb9c82b0cc10c11dced5c552450
ángel Alcantar <rayoalcantar@gmail.com>
2024-04-06 10:30:20 -0600

    Se aregla el installtask

--- addon/installTasks.py
+++ addon/installTasks.py
@@ -1,35 +1,28 @@
 ﻿# -*- coding: utf-8 -*-
-
+# TimeCalculator es un complemento para calcular el tiempo.
 # Este archivo está cubierto por la Licencia Pública General de GNU.
+# Consulta el archivo COPYING para más detalles.
 # Última actualización 2024
 # Derechos de autor (C) 2024 Ángel Alcántar <rayoalcantar@gmail.com>
 
 import addonHandler
-
-_N = _
 addonHandler.initTranslation()
 
 class donate:
     def open():
-        import languageHandler
         import webbrowser
-        lang = languageHandler.getLanguage()
-        # This check ensures the donation page is presented in Spanish if applicable.
-        if lang.startswith('es'):
-            lang = 'es'
-        else:
-            lang = 'en'
-        webbrowser.open(f"https://www.paypal.com/paypalme/rayoalcantar?lang={lang}")
+        # Directamente abrir la página de PayPal sin especificar el idioma.
+        webbrowser.open("https://www.paypal.com/paypalme/rayoalcantar")
 
     def request():
         import wx
         import gui
         
         # Translators: The title of the dialog requesting donations from users.
-        title = _N("Por favor, dona")
+        title = _("Por favor, dona")
         
         # Translators: The text of the donate dialog
-        message = _("""Internet usage monitor  - complemento gratuito para NVDA.
+        message = _("""Internet Usage Monitor - complemento gratuito para NVDA.
 Puedes hacer una donación a Ángel Alcántar para ayudar en el desarrollo futuro de este complemento.
 ¿Quieres hacer una donación ahora? Para la transacción, serás redirigido al sitio web de PayPal.""")
         
@@ -41,7 +34,5 @@ Puedes hacer una donación a Ángel Alcántar para ayudar en el desarrollo futur
 
 def onInstall():
     import globalVars
-    # This checks if NVDA is running in a secure mode (e.g., on the Windows login screen),
-    # which would prevent the addon from performing certain actions.
     if not globalVars.appArgs.secure:
         donate.request()
